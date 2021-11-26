94c9f9416222ae93c5ac66a16c5023b090e04165
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-11-26 10:21:22 +0100

    python practice

---
+++ app.py
@@ -0,0 +1,24 @@
+# print("Abiodun Oluwapelumi")
+# print("0----")
+# print(" ||||")
+# print("*" * 10)
+
+# Variable Data types
+price = 20  # int
+rating = 4.9  # float
+name = 'Amos'  # str
+is_published = False  # boolean
+
+# simple application for a Hospital.
+patientName = 'John Smith'
+patientAge = 20
+is_a_new_patient = True
+
+# Using the input() function
+name = input("What is your name? ")
+print('Hi ' + name)
+favColor = input("What is your Favorite Color? ")
+print(name + ' likes ' + favColor)
+
+# Asking for the birth year to calculate the year
+
